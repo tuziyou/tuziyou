664b2bfe3cea795ed50137f03656747aa282acfe
tuziyou <1179823436@qq.cpm>
2013-09-17 20:23:03 +0800

    Hello world
    c++1

---
+++ Helloworld.cpp
@@ -0,0 +1,13 @@
+// Helloworld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include"iostream"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"Hello World! I'm ÓÈçâæº£¡";
+	return 0;
+}
+
